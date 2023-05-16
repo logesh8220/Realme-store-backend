94f69c72c5f9469b1fc2f0388287b43c1b61ed8c
logesh8220 <logeshthirumurugan@gmail.com>
2023-05-16 16:52:02 +0530

    SOCKET

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const URL = process.env.DB;
 const DB = "realme";
 const io = require('socket.io')(3000, {
   cors: {
-    origin:"https://neon-otter-16f6a4.netlify.app",
+    origin:process.env.CLIENT,
   },
 })
 io.on("connection", socket => {
